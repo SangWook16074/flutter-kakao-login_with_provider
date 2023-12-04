011bafc8fd83cc5a145e48ca9d6281ea33d79729
HanSangWook <qazws78941@gmail.com>
2023-12-04 21:43:42 +0900

    예외처리

--- lib/src/api/kakao_login_api.dart
+++ lib/src/api/kakao_login_api.dart
@@ -4,23 +4,40 @@ import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 class KakaoLoginApi {
   signWithKakao() {
     final UserApi api = UserApi.instance;
-    try {
-      return isKakaoTalkInstalled().then((isInstalled) async {
-        if (isInstalled) {
-          await api.loginWithKakaoTalk();
-        } else {
-          await api.loginWithKakaoAccount();
+
+    return isKakaoTalkInstalled().then((isInstalled) {
+      // 카톡이 설치되었다면
+      if (isInstalled) {
+        try {
+          // 카톡으로 로그인
+          api.loginWithKakaoTalk().then((_) {
+            return api.me();
+          });
+        } catch (e) {
+          // 의도적인 뒤로가기 예외처리
+          if (e is PlatformException && e.code == "CANCELED") {
+            return;
+          }
+          // 카톡에 로그인되어있지 않으면 계정으로 로그인
+          try {
+            api.loginWithKakaoAccount().then((_) {
+              return api.me();
+            });
+          } catch (e) {
+            print(e);
+          }
         }
-      }).then((_) async {
-        final User user = await api.me();
-        print(user);
-        return user;
-      });
-    } catch (e) {
-      if (e is PlatformException && e.code == "CANCELED") {
-        return;
+      } else {
+        // 카톡이 설치되어있지 않으면
+        // 계정으로 로그인
+        try {
+          api.loginWithKakaoAccount().then((_) {
+            return api.me();
+          });
+        } catch (e) {
+          print(e);
+        }
       }
-      throw Exception("Kakao Login Faield...");
-    }
+    });
   }
 }
